ba854726b7d307a013d76003f009d85cad8f7be2
Rian Carr <ryancarr13@gmail.com>
2023-09-12 02:39:46 +0100

    Added super basic groundwork functionality for user input

--- generate_input.py
+++ generate_input.py
@@ -3,12 +3,35 @@
 #the list is in the form of a .txt file, which is then read in by generate_stats and used as input
 #this input is in the form of dictionaries which fill up for stat collection purposes
 
-#STEP 1: ask user for input
+#STEP 1: get user input
 import tkinter as tk
+
+#function to add players to the list
+def add_player():
+    input_text = user_input.get()
+    if input_text:
+        player_list.insert(tk.END, input_text)
+        user_input.delete(0, tk.END)  # Clear the input field
 
 #create the main window
 root = tk.Tk()
 root.title("Enter name of Player.")
+
+#create list of players
+player_list = tk.Listbox(root)
+player_list.pack()
+
+#create input field
+user_input = tk.Entry(root)
+user_input.pack()
+
+#create button to add input to list
+add_button = tk.Button(root, text="Add", command=add_player)
+add_button.pack()
+
+#run the file
+root.mainloop()
+
 
 
 
@@ -16,8 +39,3 @@ root.title("Enter name of Player.")
 #STEP 2: sanatise input (all lowercase and append comma)
 
 #STEP3: write into file (maybe add option to read and append to previous file in the future)
-
-
-
-#run the file
-root.mainloop()
