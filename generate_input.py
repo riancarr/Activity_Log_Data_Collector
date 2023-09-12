6b5c59e1309f83077a2aff3b6d8021d4b10f87f2
Rian Carr <ryancarr13@gmail.com>
2023-09-12 02:25:14 +0100

    Created file. Added imports.

---
+++ generate_input.py
@@ -0,0 +1,23 @@
+
+#file used to display a UI which generates a list of users and games
+#the list is in the form of a .txt file, which is then read in by generate_stats and used as input
+#this input is in the form of dictionaries which fill up for stat collection purposes
+
+#STEP 1: ask user for input
+import tkinter as tk
+
+#create the main window
+root = tk.Tk()
+root.title("Enter name of Player.")
+
+
+
+
+#STEP 2: sanatise input (all lowercase and append comma)
+
+#STEP3: write into file (maybe add option to read and append to previous file in the future)
+
+
+
+#run the file
+root.mainloop()
